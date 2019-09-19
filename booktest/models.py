2e6c5cdc93e3e5e5ab322e67fdb07bdcbeefd5e8
Ultramantoo <zhangyanfeng0307@gmail.com>
2019-09-20 07:18:01 +0800

    book_test_b

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateField()
+
+
+"""
+类名：HeroInfo
+英雄姓名：hname
+英雄性别：hgender
+英雄简介：hcomment
+英雄所属图书：hbook
+图书-英雄的关系为一对多
+"""
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcomment = models.CharField(max_length=100)
+    hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
---
+++ booktest/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from booktest import views
+urlpatterns = [
+    url(r'^$', views.index),
+]
